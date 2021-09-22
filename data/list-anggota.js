71f16a6a491d57936cac1572572d527c6d9d75bb
Ahmad Nizar <ahmad.rpl001@gmail.com>
2021-09-22 21:55:54 +0700

    add image url

--- data/list-anggota.js
+++ data/list-anggota.js
@@ -15,7 +15,7 @@ const data = [
             ig: "ikhsa",
             yt: "bla"
         },
-        image: "https://lh3.googleusercontent.com/pw/AM-JKLV6_yDC-H1139tt1b9eE_Bn_TfFiZpB_6X5Q-fbsFYKStDc6s8tDnlBR_WxiGjF0UyXk6GxrUqgAJGVe21jqTlaSNuwbu2Il9QTfPRqwX1v8pnDipqKrpN0y7HkqBLqOcN2j48LQ7FfvUnMYPUd3D6J=w1334-h889-no?authuser=0"
+        image: "https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/07/21/746412186.jpg"
     },
     {
         id: 3,
@@ -24,7 +24,7 @@ const data = [
             ig: "ikhsa",
             yt: "bla"
         },
-        image: "https://lh3.googleusercontent.com/pw/AM-JKLV6_yDC-H1139tt1b9eE_Bn_TfFiZpB_6X5Q-fbsFYKStDc6s8tDnlBR_WxiGjF0UyXk6GxrUqgAJGVe21jqTlaSNuwbu2Il9QTfPRqwX1v8pnDipqKrpN0y7HkqBLqOcN2j48LQ7FfvUnMYPUd3D6J=w1334-h889-no?authuser=0"
+        image: "https://cdn-asset.jawapos.com/wp-content/uploads/2019/01/sepintas-tentang-soeharto-yang-tumbang-20-tahun-lalu-di-tanggal-21-mei_m_214244.jpeg"
     },
     {
         id: 4,
@@ -33,7 +33,7 @@ const data = [
             ig: "ikhsa",
             yt: "bla"
         },
-        image: "https://lh3.googleusercontent.com/pw/AM-JKLV6_yDC-H1139tt1b9eE_Bn_TfFiZpB_6X5Q-fbsFYKStDc6s8tDnlBR_WxiGjF0UyXk6GxrUqgAJGVe21jqTlaSNuwbu2Il9QTfPRqwX1v8pnDipqKrpN0y7HkqBLqOcN2j48LQ7FfvUnMYPUd3D6J=w1334-h889-no?authuser=0"
+        image: "https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/01/02/3432118224.jpg"
     },
     {
         id: 5,
@@ -42,7 +42,7 @@ const data = [
             ig: "ikhsa",
             yt: "bla"
         },
-        image: "https://lh3.googleusercontent.com/pw/AM-JKLV6_yDC-H1139tt1b9eE_Bn_TfFiZpB_6X5Q-fbsFYKStDc6s8tDnlBR_WxiGjF0UyXk6GxrUqgAJGVe21jqTlaSNuwbu2Il9QTfPRqwX1v8pnDipqKrpN0y7HkqBLqOcN2j48LQ7FfvUnMYPUd3D6J=w1334-h889-no?authuser=0"
+        image: "https://cdn-2.tstatic.net/kaltim/foto/bank/images/naruto-07070.jpg"
     },
     {
         id: 6,
@@ -51,6 +51,6 @@ const data = [
             ig: "ikhsa",
             yt: "bla"
         },
-        image: "https://lh3.googleusercontent.com/pw/AM-JKLV6_yDC-H1139tt1b9eE_Bn_TfFiZpB_6X5Q-fbsFYKStDc6s8tDnlBR_WxiGjF0UyXk6GxrUqgAJGVe21jqTlaSNuwbu2Il9QTfPRqwX1v8pnDipqKrpN0y7HkqBLqOcN2j48LQ7FfvUnMYPUd3D6J=w1334-h889-no?authuser=0"
+        image: "https://animeanime.global/wp-content/uploads/2021/07/415533.jpg"
     }
 ]
